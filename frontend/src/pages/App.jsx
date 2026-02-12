bdb2fed5cc2a79e494df45df36946037adfebcc3
mpsddd-commits <mpsddd@gmail.com>
2026-02-12 16:20:55 +0900

    4시20분정리

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,14 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter, Routes, Route } from "react-router";
-
-const Home = () => {
-  return (
-    <div className="text-center">
-      <h1>메인 화면입니다.</h1>
-    </div>
-  )
-}
+import { Routes, Route } from "react-router";
+import Home from '@pages/Home.jsx'
+import Nav from '@pages/Nav.jsx'
 
 const NotFound = () => {
   return (
@@ -26,11 +20,10 @@ const App = () => {
   ]
   return (
     <>
-      <BrowserRouter>
+        <Nav />
         <Routes>
           { paths?.map((v, i) => <Route key={i} path={v.path} element={v.element} />) }
         </Routes>
-      </BrowserRouter>
     </>
   )
 }
