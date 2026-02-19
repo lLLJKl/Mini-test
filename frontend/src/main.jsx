0b8c0c6256bbefd3c26ef2591d387428979c8ab9
leeej9801-max <leeej9801@gmail.com>
2026-02-19 14:17:29 +0900

    dfdf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import App from '@pages/App.jsx'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import AuthProvider from '@hooks/AuthProvider.jsx'
 import { CookiesProvider } from 'react-cookie'
 
